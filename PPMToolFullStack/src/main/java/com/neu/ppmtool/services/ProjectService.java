7617f68a0b6aa9b24a4a52e11b5dfd64ee7751ef
joshi-aditya <joshi.adit@husky.neu.edu>
2019-05-25 19:16:37 -0400

    Added CRUD operations to spring boot app

--- PPMToolFullStack/src/main/java/com/neu/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/neu/ppmtool/services/ProjectService.java
@@ -22,4 +22,25 @@ public class ProjectService {
 
     }
 
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+ projectId +"' doesn't exists");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+ projectId +"' doesn't exists");
+        }
+        projectRepository.delete(project);
+    }
+
 }
--- PPMToolFullStack/src/main/java/com/neu/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/neu/ppmtool/web/ProjectController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
@@ -24,7 +20,6 @@ public class ProjectController {
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
 
-
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
@@ -33,5 +28,22 @@ public class ProjectController {
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() { return projectService.findAllProjects(); }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProjectById(@PathVariable String projectId){
+
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID'"+projectId+"'deleted", HttpStatus.OK);
     }
 }
