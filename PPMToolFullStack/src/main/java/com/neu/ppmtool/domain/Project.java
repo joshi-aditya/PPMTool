520de3cd821f5df3ff6c7fb0e1afb7efb1fadcba
joshi-aditya <joshi.adit@husky.neu.edu>
2019-08-19 19:28:08 -0400

    Added CRUD functionality for backlog and project tasks in backend

--- PPMToolFullStack/src/main/java/com/neu/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/neu/ppmtool/domain/Project.java
@@ -1,6 +1,7 @@
 package com.neu.ppmtool.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -34,6 +35,10 @@ public class Project {
     private Date created_at;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_at;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+    @JsonIgnore
+    private Backlog backlog;
 
     public Long getId() {
         return id;
@@ -97,6 +102,14 @@ public class Project {
 
     public void setUpdated_at(Date updated_at) {
         this.updated_at = updated_at;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     public Project() {
---
+++ PPMToolFullStack/src/main/java/com/neu/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.neu.ppmtool.repositories;
+
+import com.neu.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
